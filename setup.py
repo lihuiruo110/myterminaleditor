c49549f91921fad20f0fc6446d417b5b0b892409
lihuiruo110 <hsfreshlight@gmail.com>
2025-05-14 22:33:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,27 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='myterminaleditor',
-    version='0.1.0',
-    author='lihuiruo',
-    author_email='lihuiruo@gmail.com',  # 본인의 이메일로 수정
-    description='A custom web-based terminal and code editor module',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/lihuiruo/myterminaleditor',
-    packages=find_packages(),  # myterminaleditor 폴더 포함
+    version='1.0.0',
+    packages=find_packages(),
     include_package_data=True,
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.7',  # Python 버전
     install_requires=[
-        'flask',  # 필요에 따라 Flask나 다른 웹 프레임워크 추가
-        # 필요한 다른 패키지 추가
+        'Flask==2.1.1',
+        'xterm==5.3.0',
+        'codemirror==5.65.2',
+        'requests==2.26.0',
     ],
     entry_points={
         'console_scripts': [
-            'myterminal = myterminaleditor.cli:main',  # 커맨드라인 진입점 (선택)
+            'run-server=myterminaleditor.app:run_server',  # app.py의 run_server 함수 호출
         ],
     },
 )
